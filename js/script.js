1f93120eaabb4ca9bba162febb3e9794513dcfef
yyu4nn <Juanfernando23061@outlook.com>
2023-04-29 21:50:28 -0500

    simplous hice enojar a Dios

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 var nav = document.querySelector('nav');
 
+
+
     window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 550) {
-        nav.classList.add('bg-dark', 'shadow');
-    }else{
-        nav.classList.remove('bg-dark', 'shadow');
-    }
-    });
+        if (window.pageYOffset > 550) {
+            nav.classList.add('bg-color', 'shadow', 'texto-blanco');
+        }else{
+            nav.classList.remove('bg-color', 'shadow', 'texto-blanco');
+        }
+        });
+
+var a = document.querySelector('a');
+
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset < 550) {
+            a.classList.add('texto-negro');
+        }else{
+            a.classList.remove('texto-negro');
+        }
+        });
+
