4a7b34b1da76156ee2567ca3e70d13e9907b7eab
yyu4nn <Juanfernando23061@outlook.com>
2023-04-30 11:48:49 -0500

    segunda intento de navbar lol D:

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var nav = document.querySelector('nav');
     window.addEventListener('scroll', function () {
         if (window.pageYOffset > 550) {
-            nav.classList.add('bg-color', 'shadow', 'texto-blanco');
+            nav.classList.add('navbar-scrolled', 'shadow', 'texto-blanco');
         }else{
-            nav.classList.remove('bg-color', 'shadow', 'texto-blanco');
+            nav.classList.remove('navbar-scrolled', 'shadow', 'texto-blanco');
         }
         });
+
+
 
 
 
