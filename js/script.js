28bbcae102bce22883eed899574c0410cd14fa47
yyu4nn <Juanfernando23061@outlook.com>
2023-05-02 19:57:29 -0500

    incluidas hojas css con media queries

--- js/script.js
+++ js/script.js
@@ -21,6 +21,45 @@ var nav = document.querySelector('nav');
 
 
 
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+// Botón que activó el modal
+var button = event.relatedTarget
+// Extraer información de los atributos data-bs-*
+var recipient = button.getAttribute('data-bs-whatever')
+// Si es necesario, puedes iniciar una solicitud AJAX aquí
+// y luego realiza la actualización en una devolución de llamada.
+//
+// Actualizar el contenido del modal.
+var modalTitle = exampleModal.querySelector('.modal-title')
+var modalBodyInput = exampleModal.querySelector('.modal-body input')
+        
+modalTitle.txtContent = 'Nuevo mensaje para ' + recipient
+modalBodyInput.value = recipient
+})
+
+
+
+const carrusel = document.querySelector(".carrusel-items");
+
+let maxScrollleft = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo = null;
+let step = 2;
+const start = () => {
+    intervalo = setInterval(function () {
+        carrusel.scrollleft = carrusel.scrollleft + step;
+        if (carrusel.scrollleft === maxScrollleft) {
+            step = -1;
+        }
+
+    }, 10);
+    
+}
+
+
+const stop = () => {};
+
+start();
 
 
 
